496a29ad2d6c529eca34833a0ab116ba4382e5b5
GuleriaPiyush <139349791+GuleriaPiyush@users.noreply.github.com>
2025-09-08 00:11:25 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, request,render_template
+
+app=Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+
+
+
+
+@app.route('/calorie_calculator', methods=['GET','POST'])
+def calculateBmr():
+    gender = request.form['gender']
+    age = float(request.form['age'])
+    weight = float(request.form['weight'])
+    height = float(request.form['height'])
+
+
+
+    if gender.lower()=='m':
+        bmr = 10 * weight + 6.25 * height - 5 * age + 5
+
+    elif gender.lower()=='f':
+        bmr = 10 * weight + 6.25 * height - 5 * age - 161
+
+    else:
+        raise ValueError('Gender must be m or f ')
+    
+    return render_template("index.html",result=bmr)
+    
+    
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
